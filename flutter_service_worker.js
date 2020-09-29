2de94688d443d5253c31e24e98cf76a500234ca0
Abu Muhsin <shittuabdulmujeeb@gmail.com>
2020-09-29 01:26:08 -0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -211,7 +211,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "27fd5dbdb2a4c692f2ab98a7cb6b8c0f",
 "/": "27fd5dbdb2a4c692f2ab98a7cb6b8c0f",
-"main.dart.js": "c571691f33ed0471c528b3450f286567",
+"main.dart.js": "881d0e8edc1141c0bb3c77e836048f53",
 "manifest.json": "ab03bd3dc0841ad7cf3f49b9b15b28d1"
 };
 
